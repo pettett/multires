8e669da9428593e55fa1e67a7f78edab69eaf306
Maxwell Pettett <43613592+pettett@users.noreply.github.com>
2023-12-23 16:00:42 +0000

    Delete old vulkano project

--- vk_renderer/src/vertex.rs
+++
@@ -1,8 +0,0 @@
-use vulkano::buffer::BufferContents;
-
-#[derive(BufferContents, vulkano::pipeline::graphics::vertex_input::Vertex)]
-#[repr(C)]
-pub struct PosVertex {
-    #[format(R32G32B32_SFLOAT)]
-    position: [f32; 3],
-}
