2c5582982d1cb5660d01ecc5b4e0663582225af7
Maxwell <43613592+pettett@users.noreply.github.com>
2024-02-02 11:59:25 +0000

    Move mod.rs files to named folder files

---
+++ ash_renderer/src/utility.rs
@@ -0,0 +1,25 @@
+//! The utility mod define some fixed function using in this tutorial.
+//! Help to simplify the code.
+pub mod buffer;
+pub mod constants;
+pub mod debug;
+pub mod device;
+pub mod image;
+pub mod instance;
+mod macros;
+pub mod physical_device;
+mod pipeline;
+pub mod platforms;
+pub mod pooled;
+pub mod render_pass;
+pub mod structures;
+pub mod surface;
+pub mod swapchain;
+pub mod sync;
+pub mod tools;
+
+pub use pipeline::ComputePipeline;
+pub use pipeline::GraphicsPipeline;
+pub use pipeline::ShaderModule;
+
+// Macros for common wrapper implementations
